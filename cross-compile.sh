e405ce6cb279c58fa1ff1440492ae2f535b2de25
dakata1337 <poshtazaprezentaciaa@gmail.com>
2023-04-19 21:50:44 +0300

    docs: info on cross platform compilation

--- cross-compile.sh
+++ cross-compile.sh
@@ -1,21 +1,9 @@
 #!/bin/sh
 set -e
 
-die() {
-    printf "\x1b[31;1mERROR\x1b[0m: $1\n"
-    exit 1
-}
-
 build_for() {
     TOOLCHAIN=$1
     EXE=$2
-
-    if [[ -z "$TOOLCHAIN" ]]; then
-        die "no toolchain specified to build_for()"
-    fi
-    if [[ -z "$EXE" ]]; then
-        die "no output file specified to build_for()"
-    fi
 
     rustup target add $TOOLCHAIN
     cargo build --target "$TOOLCHAIN" --release
